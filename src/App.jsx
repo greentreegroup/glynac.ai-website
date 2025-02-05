84543b2f813ca13f232c29d80d0aca0de93e97cb
tominister <tommyfang2004@gmail.com>
2025-02-05 11:21:53 -0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
+import React from 'react';
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-import { Navbar } from "@/widgets/layout";
-import routes from "@/routes";
-
+import routes from "@/routes"; // Ensure routes are properly imported
+import Layout from './widgets/layout/Layout'; // Layout wrapper for consistent layout
 
 function App() {
-  const { pathname } = useLocation();
 
   return (
     <>
-      {!(pathname == '/sign-in' || pathname == '/sign-up') && (
-        <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
-          <Navbar routes={routes} />
-        </div>
-      )
-      }
-      <Routes>
-        {routes.map(
-          ({ path, element }, key) =>
-            element && <Route key={key} exact path={path} element={element} />
-        )}
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
+
+      {/* Define Routes within Layout */}
+      <Layout>
+        <Routes>
+          {routes.map(({ path, element }, key) => (
+            <Route key={key} path={path} element={element} />
+          ))}
+          {/* Default Route */}
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
