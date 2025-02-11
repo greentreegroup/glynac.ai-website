fd6394de68f552a9e1ea16b9d101a115b374165d
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-02-10 23:37:33 -0500

    software pages

---
+++ src/pages/software/communication-tools.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from "@material-tailwind/react";
+
+export function CommunicationTools() {
+  return (
+    <div>
+      <h1>Communication Tools</h1>
+      <p>This is the communication tools page content.</p>
+    </div>
+  );
+}
+
+export default CommunicationTools;
---
+++ src/pages/software/software.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@material-tailwind/react";
+
+export function Software() {
+  return (
+    <div>
+      <h1>Software</h1>
+      <p>This is the software page content.</p>
+    </div>
+  );
+}
+
+export default Software;
