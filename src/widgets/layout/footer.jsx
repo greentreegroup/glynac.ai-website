2e0eafdf95939a610d00a720abfaa31dcdf8f6f9
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-02-11 19:09:01 +0700

    Delete src directory

--- src/widgets/layout/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white py-6 text-center">
-      <p>&copy; {new Date().getFullYear()} Glynac.ai. All rights reserved.</p>
-    </footer>
-  );
-};
