8532632263328eca25e6e07ff3b5ebbd9088e6cc
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-02-10 23:40:51 -0500

    Add files via upload

---
+++ src/pages/ai-analysis/ai-qualitative.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from "@material-tailwind/react";
+
+export function Qualitative() {
+  return (
+    <div>
+      <h1>Qualitative</h1>
+      <p>This is the page content.</p>
+    </div>
+  );
+}
+
+export default Qualitative;
+
+
