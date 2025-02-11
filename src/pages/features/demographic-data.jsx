924b3a44e4378fef7f60e9d8a26be9bcd6bcf98f
greentreegroup <86478969+greentreegroup@users.noreply.github.com>
2025-02-10 23:38:31 -0500

    Add files via upload

---
+++ src/pages/features/demographic-data.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from "@material-tailwind/react";
+
+export function DemographicData() {
+  return (
+    <div>
+      <h1>DemographicData</h1>
+      <p>This is the page content.</p>
+    </div>
+  );
+}
+
+export default DemographicData;
