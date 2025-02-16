a56c26ac4e3cc26a611a562be87c0925dcc60b0e
DavidTheAnalyst <155424498+DavidTheAnalist@users.noreply.github.com>
2025-02-16 06:39:33 +0100

    Added White.webp

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,11 +3,39 @@ import React from "react";
 
 const Contacts = () => {
   return (
-    <div>
-      <h1>Contacts</h1>
-      <p>This is the new page you created!</p>
+    <div
+      className="relative h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/img/photo-Black background.avif')", 
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div
+        className="absolute text-white text-4xl lg:text-6xl font-bold text-center"
+        style={{
+          top: "40%", 
+          left: "50%", 
+          transform: "translate(-50%, -50%)", 
+        }}
+      >
+        <h1>Contact Us</h1>
+      </div>
+      <div
+        className="absolute bg-white w-full"
+        style={{
+          top: "50%", // Adjust this percentage as needed to control the position below the text
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "2rem", // Add padding to control the spacing inside the white background
+        }}
+      >
+        {/* Add any content here like a contact form or additional information */}
+        <p className="text-center text-black">Feel free to reach out to us!</p>
+      </div>
     </div>
   );
 };
 
 export default Contacts;
+   
+
